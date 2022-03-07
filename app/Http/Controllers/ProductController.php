9ab5a77cd7f115423f0cf5dfcc09d795fb576f9e
isah labbo <isahlabbo22@gmail.com>
2022-03-07 17:06:21 +0100

    add file

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Product;
+use App\Services\Upload\FileUpload;
 
 class ProductController extends Controller
 {
